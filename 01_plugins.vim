b0f45a2511c2db59197bbc42e5c3986b6fe8606c
Zhdanov <evdev34@gmail.com>
2025-02-16 20:50:20 +0300

    cleanup

--- 01_plugins.vim
+++ 01_plugins.vim
@@ -4,35 +4,19 @@ call vundle#begin()
 
 Plugin 'VundleVim/Vundle.vim'
 Plugin 'tpope/vim-fugitive'
-Plugin 'git://git.wincent.com/command-t.git'
-Plugin 'rstacruz/sparkup', {'rtp': 'vim/'}
-Plugin 'ascenator/L9', {'name': 'newL9'}
 Plugin 'ervandew/supertab'
 Plugin 'tpope/vim-endwise'
-Plugin 'tpope/vim-surround'
-"Plugin 'yous/vim-open-color'
 Plugin 'eldritch-theme/eldritch.nvim'
-Plugin 'vim-scripts/vim-auto-save'
 Plugin 'tpope/vim-commentary'
-Plugin 'vim-scripts/matchit.zip'
-Plugin 'vim-scripts/ruby-matchit'
 Plugin 'vim-airline/vim-airline'
 Plugin 'slim-template/vim-slim'
-Plugin 'ngmy/vim-rubocop'
-Plugin 'tpope/vim-dispatch'
-Plugin 'embear/vim-localvimrc'
 
 Plugin 'elixir-editors/vim-elixir'
 Plugin 'vim-ruby/vim-ruby'
 Plugin 'tpope/vim-rails'
-Plugin 'tpope/vim-bundler'
-Plugin 'pangloss/vim-javascript'
 Plugin 'elzr/vim-json'
 Plugin 'maxmellon/vim-jsx-pretty'
-Plugin 'HerringtonDarkholme/yats.vim'
 Plugin 'dense-analysis/ale'
-Plugin 'posva/vim-vue'
-Plugin 'hashivim/vim-terraform'
 Plugin 'github/copilot.vim'
 Plugin 'kdheepak/lazygit.nvim'
 
@@ -41,7 +25,6 @@ Plugin 'nvim-neo-tree/neo-tree.nvim'
 Plugin 'nvim-tree/nvim-web-devicons'
 Plugin 'nvim-lua/plenary.nvim'
 Plugin 'MunifTanjim/nui.nvim'
-Plugin '3rd/image.nvim'
 
 call vundle#end()
 
--- 08_setup.lua
+++ 08_setup.lua
@@ -30,9 +30,6 @@ require("neo-tree").setup({
   sources = { "filesystem", "buffers", "git_status", "document_symbols" }
 })
 
--- zsh
--- vim.opt.shell = '/bin/zsh -l'
-
 -- fzf
 vim.cmd [[
   set rtp+=/usr/local/opt/fzf
@@ -40,42 +37,3 @@ vim.cmd [[
 
 -- ripgrep
 vim.opt.grepprg = 'rg --color=never'
-
--- 3rd/image preview images
-require("image").setup({
-  backend = "kitty",
-  processor = "magick_cli", -- or "magick_rock"
-  integrations = {
-    markdown = {
-      enabled = true,
-      clear_in_insert_mode = false,
-      download_remote_images = true,
-      only_render_image_at_cursor = false,
-      floating_windows = false, -- if true, images will be rendered in floating markdown windows
-      filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
-    },
-    neorg = {
-      enabled = true,
-      filetypes = { "norg" },
-    },
-    typst = {
-      enabled = true,
-      filetypes = { "typst" },
-    },
-    html = {
-      enabled = false,
-    },
-    css = {
-      enabled = false,
-    },
-  },
-  max_width = nil,
-  max_height = nil,
-  max_width_window_percentage = nil,
-  max_height_window_percentage = 50,
-  window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
-  window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "snacks_notif", "scrollview", "scrollview_sign" },
-  editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
-  tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
-  hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, -- render image files as images when opened
-})
