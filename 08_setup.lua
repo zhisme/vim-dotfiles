354bf650f5ccb4274c24817528b7da7d5306aab0
Evgeny Zhdanov <evdev34@gmail.com>
2025-04-21 11:22:57 +0300

    add copy_with_context

--- 08_setup.lua
+++ 08_setup.lua
@@ -37,3 +37,15 @@ vim.cmd [[
 
 -- ripgrep
 vim.opt.grepprg = 'rg --color=never'
+
+-- copy_with_context
+require('copy_with_context').setup({
+  -- Customize mappings
+  mappings = {
+    relative = '<leader>cy',
+    absolute = '<leader>cY'
+  },
+  -- whether to trim lines or not
+  trim_lines = false,
+  context_format = '# %s:%s', -- Default format for context: "# Source file: filepath:line"
+})
